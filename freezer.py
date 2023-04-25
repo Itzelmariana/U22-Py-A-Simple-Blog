ab622db801f5736d8a7d59bf66b0db868d48c3be
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-04-24 20:48:33 -0500

    add build

--- freezer.py
+++ freezer.py
@@ -1,7 +1,24 @@
 from flask_frozen import Freezer
 from main import app
+import requests
 
 freezer = Freezer(app)
+
+
+@freezer.register_generator
+def contact():
+    yield '/contact'
+
+
+@freezer.register_generator
+def about():
+    yield '/about'
+
+
+@freezer.register_generator
+def get_blog():
+    yield '/post.html'
+
 
 if __name__ == '__main__':
     freezer.freeze()
